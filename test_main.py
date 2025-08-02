a2a32ee3a72c16d7b31bd97607f72de83a181b18
Ahmad Shareef <ahmadbinshareef@gmail.com>
2025-08-02 16:14:59 +0530

    initial python ci with multi

--- test_main.py
+++ test_main.py
@@ -1,5 +1,26 @@
-from main import add
+from main import add, subtract, multiply, divide
+
 
 def test_add():
     assert add(2, 3) == 5
-    assert add(0, 0) == 0
+
+
+def test_subtract():
+    assert subtract(5, 2) == 3
+
+
+def test_multiply():
+    assert multiply(3, 4) == 12
+
+
+def test_divide():
+    assert divide(10, 2) == 5
+
+
+def test_divide_by_zero():
+    try:
+        divide(10, 0)
+        assert False  # This should not be reached
+    except ValueError:
+        assert True
+

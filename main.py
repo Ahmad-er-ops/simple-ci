0126d37e0472ef355953428ce5275dcc279a8ca4
Ahmad Shareef <ahmadbinshareef@gmail.com>
2025-08-07 15:37:27 +0530

    djklsjfkdsjf

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
-# main.py
-
-from flask import Flask, request
+from flask import Flask, request, render_template_string
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Welcome to the Calculator!"
+@app.route("/")
+def calculator():
+    return '''
+        <form method="POST" action="/result">
+            Number 1: <input type="text" name="num1"><br>
+            Number 2: <input type="text" name="num2"><br>
+            <input type="submit" value="Calculate">
+        </form>
+    '''
 
-@app.route('/add')
-def add():
-    try:
-        a = int(request.args.get('a', 0))
-        b = int(request.args.get('b', 0))
-        return f"Result: {a + b}"
-    except:
-        return "Error: Invalid input"
+@app.route("/result", methods=["POST"])
+def result():
+    num1 = int(request.form["num1"])
+    num2 = int(request.form["num2"])
+    total = num1 + num2
+    return f"<h2>Result: {total}</h2>"
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
---
+++ webhook.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+import os
+import git
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    repo = git.Repo('/home/Ahmad-er-ops/simple-ci')
+    origin = repo.remotes.origin
+    origin.pull()
+    os.system("pkill -f app.py")
+    os.system("nohup python3 /home/your-username/auto-deploy-demo/app.py &")
+    return 'Updated Python App\n'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
+
